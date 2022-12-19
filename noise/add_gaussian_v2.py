c16419ed0f578d2a6919d55d4d93e2d5048839ec
hentci <1222bruce@gmail.com>
2022-12-20 00:55:02 +0800

    [add] probably useful

--- noise/add_gaussian_v2.py
+++ noise/add_gaussian_v2.py
@@ -2,19 +2,29 @@ import cv2
 import numpy as np
 
 # Load the image
-image = cv2.imread("image.jpg")
+image = cv2.imread("../test_img/doge.jpg")
 
-# Convert the image to the RGB color space
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# Convert the image to a numpy array
+image_array = np.array(image)
 
-# Generate random noise with a mean of 0 and standard deviation of 50
-noise = np.random.normal(0, 50, image.shape)
+# Split the image into separate color channels
+red_channel, green_channel, blue_channel = cv2.split(image_array)
 
-# Add the noise to the image
-image = image + noise
+# Generate noise matrices for each color channel with standard deviation 0.5
+red_noise = np.random.normal(0, 0.9, red_channel.shape)
+green_noise = np.random.normal(0, 0.9, green_channel.shape)
+blue_noise = np.random.normal(0, 0.9, blue_channel.shape)
 
-# Clip the pixel values to the valid range (0-255)
-image = np.clip(image, 0, 255).astype(np.uint8)
+# Add the noise to each color channel
+noisy_red = red_channel + red_noise
+noisy_green = green_channel + green_noise
+noisy_blue = blue_channel + blue_noise
+
+# Combine the noisy color channels into a single image
+noisy_image = cv2.merge([noisy_red, noisy_green, noisy_blue])
+
+# Convert the noisy image back to an image and save it
+noisy_image = np.uint8(noisy_image)
 
 # Save the resulting noisy image
-cv2.imwrite("noisy_image.jpg", image)
+cv2.imwrite("../test_img/noisy_image.jpg", noisy_image)
